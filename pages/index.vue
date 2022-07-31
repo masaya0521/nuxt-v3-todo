5fc214daf88380845e4836b8e2108543997e7e6b
masaya0521 <kaji0521km@gmail.com>
2022-07-31 18:16:59 +0900

    pageのディレクトリ変更

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { todoList } = useTodo();
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <div class="flex flex-col">
+      <span class="text-gray-800 text-4xl my-16">ToDo List</span>
+      {{ todoList }}
+    </div>
+  </div>
+</template>
